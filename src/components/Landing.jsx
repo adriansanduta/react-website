af95de8b6e88b55245444806107d473bbd0303b9
Adrian <adriansanduta@gmail.com>
2025-01-05 15:18:01 -0600

    1.6

---
+++ src/components/Landing.jsx
@@ -0,0 +1,29 @@
+
+import React from 'react';
+import UndrawBooks from '../assets/undraw_Books.svg';
+
+const Landing = () => {
+    return (
+        <section id='landing'>
+        <header>
+            <div className="headr__container">
+                <div className="header__description">
+                    <h1>America's most awarded online library platform</h1>
+                    <h2>Find your dream book with <span className='purple'>Library</span></h2>
+                    <a href="#features">
+                    <button className="btn">Browse books</button>
+                    </a>
+                </div>
+                <figure className="header__image--wrapper">
+                  <img src={UndrawBooks} alt="" />
+                </figure>
+            </div>
+        </header>
+        </section>
+    );
+}
+
+export default Landing;
+
+
+
