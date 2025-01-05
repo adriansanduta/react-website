6f848089bfa864fcc5a87248ffe6296ebaaa5f8c
Adrian <adriansanduta@gmail.com>
2025-01-04 22:22:11 -0600

    1.4

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import './App.css'
-import Nav from "./components/Nav";
 import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 
+import Nav from "./components/Nav.jsx";
 
 function App() {
   return (
    <div className="App">
-    <Nav></Nav>
+    <Nav />
    </div>
   );
 }
 
 export default App;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
