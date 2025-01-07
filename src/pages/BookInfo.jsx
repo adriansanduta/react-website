efe365b30b53e2d44e27fe99fbb41c5390992b0e
Adrian <adriansanduta@gmail.com>
2025-01-06 20:45:03 -0600

    3.4

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -12,6 +12,17 @@ const BookInfo = ({books}) => {
                     <Link to="/books" className="book__link">
                     <FontAwesomeIcon icon="arrow-left" />
                     </Link>
+                    <Link to="/book" className="book__link">
+                    <h2 className="book__selected--title--top">Books</h2>
+                    </Link>
+                </div>
+                <div className="book__selected">
+                    <figure className="book__selected--figure">
+                        <img src="https://m.media-amazon.com/images/I/61mIq2iJUXL._AC_UF1000,1000_QL80_.jpg" alt="" className="book__selected--img" />
+                    </figure>
+                    <div className="book__selected--description">
+                        
+                    </div>
                 </div>
             </div>
         </div>
