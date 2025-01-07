7b30fda5c5b9858d604b9cb66ec8ea4f9582e123
Adrian <adriansanduta@gmail.com>
2025-01-06 18:14:40 -0600

    2.8

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Landing from "./components/Landing.jsx";
 import Nav from "./components/Nav.jsx";
-import Highlights from './components/Highlights.jsx';
-import Featured from './components/Featured.jsx';
-import Discounted from './components/Discounted.jsx';
-import Explore from './components/Explore.jsx';
 import Footer from './components/Footer.jsx';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
 
 function App() {
   return (
     <Router>
    <div className="App">
-    <Route />
     <Nav />
-    <Landing />
-    <Highlights />
-    <Featured /> 
-     <Discounted />
-     <Explore />
+    <Route />
+    <Home />
      <Footer />
    </div>
    </Router>
