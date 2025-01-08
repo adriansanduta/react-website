6b3ac5e7c251a981cd223ae46bfe7381d53d0c82
Adrian <adriansanduta@gmail.com>
2025-01-07 20:48:04 -0600

    4.2

--- src/App.js
+++ src/App.js
@@ -16,8 +16,20 @@ function App() {
   function addToCart(book) {
     const dupeItem = cart.find(item => +item.id === +book.id)
    if (dupeItem) {
-    dupeItem.quantity += 1;}
-  setCart([...cart, {...book, quantity: 1}])
+    setCart(cart.map(item => {
+     if  (item.id === dupeItem.id) {
+      return {
+        ...item, quantity: item.quantity +1,
+      }
+     }
+     else {
+      return item
+     }
+    }))
+   }
+   else {
+    
+   }
   }
 
   useEffect(() => {}, [cart]);
